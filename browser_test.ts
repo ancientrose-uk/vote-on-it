36e42d904039cd9c2789bd4061ace01eaffb5f88
Natalie Carey <natalie@ancientrose.uk>
2025-05-05 16:25:40 +0100

    A bit more structure.

--- browser_test.ts
+++ browser_test.ts
@@ -1,13 +1,53 @@
 import { chromium } from "playwright";
+import { expect } from "jsr:@std/expect";
+import { afterAll, describe, it } from "jsr:@std/testing/bdd";
 
-// Check if the SHOW_BROWSER environment variable is set
-const showBrowser = Deno.env.get("SHOW_BROWSER") === "true";
+type CleanupFn = () => Promise<void>;
 
-const browser = await chromium.launch({ headless: !showBrowser });
-const page = await browser.newPage();
+const cleanupFunctions: { priority: number; task: CleanupFn }[] = [];
 
-await page.goto("https://ancientrose.uk");
-const title = await page.title();
+function addCleanupTask(task: CleanupFn, priority: number = 0) {
+  cleanupFunctions.push({ task, priority });
+  cleanupFunctions.sort((a, b) => a.priority - b.priority);
+}
 
-console.log(`Page title: ${title}`);
-await browser.close();
+async function runCleanupTasks() {
+  while (cleanupFunctions.length > 0) {
+    const { task } = cleanupFunctions.shift()!;
+    await task();
+  }
+}
+
+async function getBrowserPage() {
+  const showBrowser = Deno.env.get("SHOW_BROWSER") === "true";
+  const delayBeforeClosingBrowser = Number(Deno.env.get("DELAY_BEFORE_CLOSING_BROWSER") || 0);
+
+  const browser = await chromium.launch({ headless: !showBrowser });
+  const page = await browser.newPage();
+
+  addCleanupTask(async () => {
+    await sleep(delayBeforeClosingBrowser);
+    await browser.close();
+  });
+  return { page };
+}
+
+const sleep =(ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+afterAll(async () => {
+  await runCleanupTasks();
+})
+
+describe("Browser Tests", () => {
+  it("should fail when the title is not as expected", async () => {
+    // Check if the SHOW_BROWSER environment variable is set
+    const { page } = await getBrowserPage();
+
+    await page.goto("https://ancientrose.uk");
+    const title = await page.title();
+
+    // Expect the title to be a specific incorrect value to test failure mode
+    expect(title).toBe("this is not the actual title (testing failure mode)");;
+
+  });
+});
