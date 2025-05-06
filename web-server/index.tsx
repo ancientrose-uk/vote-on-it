d6bd2d7e4716cb527803237da7ae2657f3b7d556
Amyas Merivale <merivale@gmail.com>
2025-05-06 10:07:47 +0200

    Add React SSR

--- web-server/index.tsx
+++ web-server/index.tsx
@@ -1,11 +1,16 @@
+import React from "react";
+import { renderToString } from "https://esm.sh/react-dom/server";
+
+const report = `Env var: ${Deno.env.get('NODE_ENV')}`
+
 Deno.serve({
   port: Deno.env.get("PORT") ? Number(Deno.env.get("PORT")) : 0,
   handler: () => {
-    return new Response("<h1>Welcome to Vote On It!</h1>", {
+    return new Response(renderToString(<h1>Welcome to Vote On It! {report}</h1>), {
       headers: {
         "Content-Type": "text/html",
       },
-    })
+    });
   },
 });
 Deno.addSignalListener("SIGINT", () => {
