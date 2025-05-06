1db702a68dccddeb98142508a4cd18f090c67a67
Natalie Carey <natalie@ancientrose.uk>
2025-05-06 15:49:09 +0100

    Basic login test (no auth logic yet)

--- system-tests/basic.spec.ts
+++ system-tests/basic.spec.ts
@@ -1,12 +1,8 @@
 import {expect} from "jsr:@std/expect";
-import {afterAll, describe, it} from "jsr:@std/testing/bdd";
-import { runCleanupTasks, startServer, getBrowserPage } from "./helpers/test_utils.ts";
+import {describe, it} from "jsr:@std/testing/bdd";
+import { startServer, getBrowserPage } from "./helpers/test_utils.ts";
 
-afterAll(async () => {
-  await runCleanupTasks();
-})
-
-describe("Browser Tests", () => {
+describe("Basic Tests", () => {
   it("should have a sensible main heading", async () => {
     const { baseUrl } = await startServer()
     const { browserFns } = await getBrowserPage(baseUrl);
