83efaf6b1bbb9d784f19d55bcf865caea57df17d
Natalie Carey <natalie@ancientrose.uk>
2025-05-07 22:27:37 +0100

    Added my git hook script with some basic instructions for anyone who wants to use the same approach

---
+++ scripts/natalies-pre-commit-hook.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# If you want to use this run:
+#
+# ln -s `pwd`/scripts/natalies-pre-commit-hook.sh .git/hooks/pre-commit
+#
+# you don't have to use this, the PR checks will run these checks for you!
+#
+# Note - this is intentionally set up to allow changes over time, you might choose not to opt-in to this.
+
+deno task check:all:fix && deno task test:unit && deno task test:browser:all && deno task test:browser:all:no-js
